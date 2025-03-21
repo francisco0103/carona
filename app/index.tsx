8223570e069806453ad429ff7e49e986eed3e34d
Francisco Raí <franciscovieira0103@gmail.com>
2025-03-21 14:01:39 -0300

    carona

--- app/index.tsx
+++ app/index.tsx
@@ -1,26 +1,42 @@
-import { View, Text, Image,Button } from 'react-native'
+import { View, Text, Image, Button,StyleSheet } from 'react-native'
 import React from 'react'
 
 const index = () => {
   return (
-    <View>
-            <Image  />
+    <View style={ styles.View}>
     
-      <Text style={{alignSelf:'center',justifyContent:'center'}}><h1 >Como você quer usar o app </h1></Text>
-      <Text style={{alignSelf:'center',justifyContent:'center'}}>Olá sou o Motorista</Text>
-      <Image style={{width:150,height:150,alignSelf:'center',justifyContent:'center'}} source={ require('../assets/driver.png')} />
+      <Text style = {styles.Text} >Como você quer usar o app </Text>
+      <Text >Olá sou o Motorista</Text>
+      <Image style={styles.Image} source={ require('../assets/driver.png')} />
      
-      <Text style={{alignSelf:'center',justifyContent:'center'}}><h1><Button title='Motorista'>Motorista</Button></h1></Text>
+      <Text ><Button title='Motorista'></Button></Text>
 
-      <Text style={{alignSelf:'center',justifyContent:'center'}}>Olá sou o Carona </Text>
+      <Text >Olá sou o Carona </Text>
       <Image style={{width:150,height:150,alignSelf:'center',justifyContent:'center'}} source={ require('../assets/Passager.png')} />
       
-      <Text style={{alignSelf:'center',justifyContent:'center'}}><h1><Button title='Motorista'>Carona</Button></h1></Text>
+      <Text ><Button title='Carona' ><h1>Carona</h1></Button></Text>
      
 
     </View>
   )
 }
+const styles = StyleSheet.create({
+
+  Text:{
+    position:'relative', fontSize: 34,
+     fontWeight:500 , 
+      paddingHorizontal: 60,
+      marginBottom: 60
+  },
+Image:{
+  width:150,height:150,alignSelf:'center',justifyContent:'center'
+},
+View:{
+flex: 1, alignItems:'center',justifyContent:'center'
+
+},
+
+})
 
 
 export default index

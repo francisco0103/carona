2fc7152549506a41bd5345ea1a8901b9a25c3a9b
Francisco Raí <franciscovieira0103@gmail.com>
2025-03-21 13:29:36 -0300

    carona

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,26 @@
-import { View, Text, Image } from 'react-native'
+import { View, Text, Image,Button } from 'react-native'
 import React from 'react'
 
 const index = () => {
   return (
     <View>
-      <Text><h1>Olá mundo</h1></Text>
+            <Image  />
+    
+      <Text style={{alignSelf:'center',justifyContent:'center'}}><h1 >Como você quer usar o app </h1></Text>
+      <Text style={{alignSelf:'center',justifyContent:'center'}}>Olá sou o Motorista</Text>
+      <Image style={{width:150,height:150,alignSelf:'center',justifyContent:'center'}} source={ require('../assets/driver.png')} />
+     
+      <Text style={{alignSelf:'center',justifyContent:'center'}}><h1><Button title='Motorista'>Motorista</Button></h1></Text>
+
+      <Text style={{alignSelf:'center',justifyContent:'center'}}>Olá sou o Carona </Text>
+      <Image style={{width:150,height:150,alignSelf:'center',justifyContent:'center'}} source={ require('../assets/Passager.png')} />
+      
+      <Text style={{alignSelf:'center',justifyContent:'center'}}><h1><Button title='Motorista'>Carona</Button></h1></Text>
+     
+
     </View>
   )
 }
+
 
 export default index
